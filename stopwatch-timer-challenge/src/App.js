7c23b104fddfd2a55ea0e1479f7409cc667e071f
Charlie Gray <charliechasegray@gmail.com>
2023-12-11 17:09:13 -0800

    final commit

--- stopwatch-timer-challenge/src/App.js
+++ stopwatch-timer-challenge/src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [timer, setTimer] = useState(0);
+
+  const startTimer = () => {
+    window.myTimer = setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, 1000);
+  };
+  const stopTimer = () => {
+    clearInterval(window.myTimer);
+  };
+  const resetTimer = () => {
+    clearInterval(window.myTimer);
+    setTimer(0);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Timer</h1>
+      <span>{Math.trunc(timer / 60)} mins </span>
+      <span>{timer % 60} secs</span>
+      <div>
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
     </div>
   );
 }
